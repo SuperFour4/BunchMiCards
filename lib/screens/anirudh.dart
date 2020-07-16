0e97ae2c728aaf5ce79d9460d529fde3520c6ae8
anirudhnagubandi <anirudh.nagubandi@gmail.com>
2020-07-16 18:17:21 +0530

    anirud

--- lib/screens/anirudh.dart
+++ lib/screens/anirudh.dart
@@ -1,18 +1,90 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 class Anirudh extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      width:100,
-      height: 100,
-      child: FlatButton(
-        child:Text('Next'),
-        onPressed: (){
-          Navigator.pushNamed(context, '/shiva');
-        },
-      )
+    return  Scaffold(
+      backgroundColor: Colors.red.shade50,
+      body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/angela.jpg'),
+              ),
+              Text(
+                'Anirudh',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 9381437372',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  )),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'anirudh.nagubandi@email.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  //margin: EdgeInsets.all(20),
+                  child:FlatButton(
+                    child:Text('Next',style: TextStyle(fontSize: 20,color:Colors.white),),
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/shiva');
+                    },
+                  ),
+                ),
+              ),
+            ],
+          )
+      ),
     );
   }
 }
@@ -20,22 +92,3 @@ class Anirudh extends StatelessWidget {
 //TODO: onPressed lo em rayalo nenu miku chepta danini empty ga vadileyandi
 //TODO: mi card interface complete ayyaka comments lo unna flat button ni micard column lo last lo pettandi
 //TODO: fonts and images miku nachinavi download chesi pettandi
-/*
-FlatButton(
-                onPressed: () {
-                },
-                child: Container(
-                  width: 80.0,
-                  color: Colors.white,
-                  child: Center(
-                    child: Text(
-                      'NEXT',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Ubuntu',
-                          fontSize: 20.0),
-                    ),
-                  ),
-                ),
-              )
- */
